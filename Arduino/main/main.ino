b4882e35aea7a54ccb9765c49c6b4d62163f2253
Krisztian Kurucz <kuruczkk@gmail.com>
2017-07-14 15:08:45 -0400

    More foundation work
    Added BLE, ESP8266, other config code based on snippets found online

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -4,6 +4,8 @@
 /* Includes */
 #include <SPI.h>
 #include <SD.h>
+#include <SparkFunESP8266WiFi.h>
+#include <CurieBLE.h>
 
 /* Pin Assignments */
 // Analog
@@ -35,6 +37,26 @@ Sd2Card card;
 SdVolume volume;
 SdFile root;
 
+// Load configuration bits from memory (hardcoded for now)
+bool firstTimeSetup = true;
+
+// Set up Curie BLE
+BLEPeripheral ble;
+/*
+Other usable UUIDs (generated @ https://www.uuidgenerator.net/)
+987234d7-e132-4c29-aa39-fbc82ba8e1ee
+b63fadc0-713e-4928-afed-a3e2fd9f86c3
+64ea688c-4699-42d3-9cfe-571f85758b61
+6e09fadf-fd17-4bf9-882b-64c8fe805452
+9fca11ce-afcf-4d96-a324-7662a61c37e5
+4a0617c6-36c6-4388-a21e-c29ddec784e7
+d33e0ebf-395c-4c65-bc64-bfee8416ab5b
+e7807690-3ce5-434e-97d2-33383ba5b23b
+fd8f58b3-205c-4189-8600-f92fcd78503a
+*/
+BLEService testService("94758d00-7a46-4ce5-a11f-10d93808580c");
+BLECharCharacteristic testCharacteristic("94758d01-7a46-4ce5-a11f-10d93808580c", BLERead | BLEWrite);
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   
@@ -59,6 +81,29 @@ void setup() {
   pinMode(trip_led_1_pin, OUTPUT);
   pinMode(trip_led_2_pin, OUTPUT);
   pinMode(trip_led_3_pin, OUTPUT);
+  
+  // Serial USB console (the one we can see)
+  Serial.begin(9600);
+  
+  // Set up ESP8266
+  if (esp8266.begin(115200, Serial1) {
+    Serial.println("ESP8266 OK");
+  }
+  else {
+    Serial.println("ESP8266 FAIL");
+  }
+      
+  // Set up BLE
+  ble.setLocalName("AirFuseBLE");
+  ble.setAdvertisedServiceUuid(testService.uuid());
+      
+  ble.addAttribute(testService);
+  ble.addAttribute(testCharacteristic);
+      
+  if (firstTimeSetup) {
+    ble.begin();
+    Serial.println("BLE ON");
+  }
   
 }
 
