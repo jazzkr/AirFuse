3bc8d153096e62dc263d4fabd02d31598c1d939b
Utkarsh Saini <utkarsh.saini@uwaterloo.ca>
2017-07-19 18:28:36 -0400

    Added a better sampling algorithm to allow reduce errors due to noise.

--- Arduino/CurrentSensing_July15/CurrentSensing_July15.ino
+++ Arduino/CurrentSensing_July15/CurrentSensing_July15.ino
@@ -7,7 +7,6 @@ float prev_current;
 float current;
 
 void setup()
-
 {
   Serial.begin(9600);          //  setup serial
   val = 0;
@@ -15,14 +14,9 @@ void setup()
   current = 0.0;
 }
 
-
-
 void loop()
-{
-  val = analogRead(analogPin);    // read the input pin
-  float voltage = val / 1023.0 * 5.0 - 0.82;
-  
-  current = voltage/0.4;
+{  
+  current = getCurrentReading();
   
   if (current > 0.1 && ((abs(prev_current - current)/prev_current)*100 > 5)){
     //Serial.println((abs(prev_current - current)/prev_current)*100);
@@ -32,3 +26,14 @@ void loop()
     prev_current = current * 1.0;
   }
 }
+
+float getCurrentReading(){
+  float voltage = 0.0;
+  for (int i = 0; i < 25; i++){
+    val = analogRead(analogPin);
+    voltage = voltage + val / 1023.0 * 5.0 - 0.82;
+  }
+  voltage = voltage / 25.0;
+  return voltage/0.4;
+}
+
